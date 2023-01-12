c0686f2ff37a70682e24820e27a8642d2ef7cbb5
kelicaleb <kelicaleb7@gmail.com>
2023-01-12 18:04:48 +0300

    Completed assignment

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,16 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import MesaVerde from "./parks/MesaVerde";
+import {trees, wildlife, elevation} from "./parks/RockyMountain"
 
 function ColoradoStateParks() {
-  howManyParks(); // => "42 parks!"
+  howManyParks();
+  console.log(trees)
+  wildlife();
+  elevation();
 
-  return <h1>Colorado State Parks!</h1>;
+  return (<><h1>Colorado State Parks!</h1><MesaVerde /></>
+  );
 }
+
+export default ColoradoStateParks;
